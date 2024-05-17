2c26ce1b5473b5664d14fbc033c6301d85d87172
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-05-16 22:00:33 -0400

    first push. It has the basic html, sass, and javascript. need to work on the javascript logic (operations and onclick functions)

---
+++ script.js
@@ -0,0 +1,6 @@
+const addIncomeBtn = document.getElementById("add-income");
+const addExpenseBtn = document.getElementById("add-expense");
+
+const displayBtn = function displayBtn() {
+    addIncomeBtn.addEventListener("onclick",)
+}
