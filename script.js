467efb9540a22eecf71f2675ab8bcb4bc9deb215
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-05-17 19:54:10 -0400

    Added javascript functionality for the add buttons. Still doesn't work the way it's intended. Need to work on adding cells in their own respective columns.

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-const addIncomeBtn = document.getElementById("add-income");
-const addExpenseBtn = document.getElementById("add-expense");
+//here I'm targeting the table body so I can add input fields with both Add+ buttons
+const inputFields = document.getElementById("inputFields")
 
-const displayBtn = function displayBtn() {
-    addIncomeBtn.addEventListener("onclick",)
+
+//I'm confirming if the html elements are inside the variables
+console.log(inputFields)
+
+
+//below is a function that will repeat
+function addIncomeInput() {
+    var newRow = document.createElement('tr');
+    var newCell = document.createElement('td');
+    newCell.innerHTML =
+        `<input type="text" id="income-description" name="income-description"
+                            placeholder="Name your income">
+                        <input type="text" id="income-number" name="income-number" placeholder="type the amount">`
+    newRow.appendChild(newCell)
+    inputFields.appendChild(newRow)
 }
+
+function addExpenseInput() {
+    var newRow = document.createElement('tr');
+    var newCell = document.createElement('td');
+    newCell.innerHTML = `
+        <input type="text" id="expenses-description" name="expenses-description"
+        placeholder="Name your expense">
+    <input type="text" id="expenses-number" name="expenses-number" placeholder="type the amount">`
+    newRow.appendChild(newCell)
+    inputFields.appendChild(newRow)
+}
+
+
+
+
+
