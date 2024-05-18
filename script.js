69a85ca5feef068a2417bcdbce2952e34bda9c7b
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-05-18 18:47:49 -0400

    Finally completed the Javascript logic behind the calculations, the submit button, and the reset button. Now it's time to implement SASS and clean the javascript code. I want to add a delete button inside all the cells that are added with the Add + button. I want to have the cells be in their own respective columns when you add them.

--- script.js
+++ script.js
@@ -1,10 +1,81 @@
+//below is the logic for the difference in income and expenses totaling the budget
+
+class Budget {
+    constructor(income = 0, expense = 0) {
+        this.income = income;
+        this.expense = expense;
+        this.total = 0;
+    }
+
+    addingIncome(income) {
+        this.income += income;
+    }
+
+    addingExpense(expense) {
+        this.expense += expense;
+    }
+
+    grandTotal() {
+        return this.total = this.income - this.expense
+    }
+
+    inputValidation() {
+        //if else statements will be used to display alerts for any income amounts that are less than the expense and for empty input fields
+        if (typeof this.income !== 'number' || typeof this.expense !== 'number') {
+            alert("Income and expense must be numbers!");
+        } if (this.income < this.expense) {
+            alert("Get your money up!")
+        } else if (this.income === 0 || this.expense === 0) {
+            alert("Must have valid number entries")
+
+        }
+    }
+}
+
+//this prevents the form from resetting when submitting
+function preventFormReset(event) {
+    event.preventDefault();
+    updateData();
+}
+
+//this will link to my submit button. Whenever values are inputted, javascript will receive them as strings. So below I added a for loop that will parse/convert each entry into a number using parseFloat. Float means any number with a decimal or a super long number, but when put next to parse, will help convert all entries into numbers. 
+function updateData() {
+    let incomeAmount = 0;
+    let expenseAmount = 0;
+
+    let incomeInputs = document.querySelectorAll("input[name='income-number']");
+    let expenseInputs = document.querySelectorAll("input[name='expenses-number']");
+
+    for (let i = 0; i < incomeInputs.length; i++) {
+        incomeAmount += parseFloat(incomeInputs[i].value) || 0;
+    }
+
+    for (let i = 0; i < expenseInputs.length; i++) {
+        expenseAmount += parseFloat(expenseInputs[i].value) || 0;
+    }
+
+    //this calculates income minus expenses
+    const grandTotalAmount = incomeAmount - expenseAmount;
+
+    //below the code will update the all the total displays from the user's input
+    document.getElementById("incomeTotal").textContent = `Total Income: ${incomeAmount}`;
+    document.getElementById("expenseTotal").textContent = `Total Expense: ${expenseAmount}`;
+    document.getElementById("grandTotal").textContent = `Grand Total: ${grandTotalAmount}`;
+}
+
+// below I'm defining the reset button to reset the form before submitting and reset the values on the display after submitting
+function resetTotals() {
+    document.getElementById("incomeTotal").innerHTML = `<em>Total Income: 0</em>`;
+    document.getElementById("expenseTotal").innerHTML = `<em>Total Expense: 0</em>`;
+    document.getElementById("grandTotal").innerHTML = `<strong>Grand Total: 0</strong>`;
+}
+
+
 //here I'm targeting the table body so I can add input fields with both Add+ buttons
 const inputFields = document.getElementById("inputFields")
 
-
 //I'm confirming if the html elements are inside the inputFields variable
 console.log(inputFields);
-
 
 //below is a function that will add income cells
 function addIncomeInput() {
@@ -29,64 +100,4 @@ function addExpenseInput() {
     // newCell.style = (<i class="material-icons" style="font-size:48px;color:red">delete</i>)
     newRow.appendChild(newCell)
     inputFields.appendChild(newRow)
-}
-
-
-
-//below is the logic for the difference in income and expenses totaling the budget
-
-class Budget {
-    constructor(income = 0, expense = 0) {
-        this.income = income;
-        this.expense = expense;
-        this.total = 0;
-    }
-
-    addingIncome(income) {
-        this.income += income;
-    }
-
-    addingExpense(expense) {
-        this.expense += expense;
-    }
-
-    grandTotal() {
-        return this.total = this.income - this.expense
-    }
-
-    inputValidation() {
-        //if else statements will be used to display alerts for any income amounts that are less than the expense and for empty input fields
-        if (this.income < this.expense) {
-            alert("Get your money up!")
-        } else if (this.income === 0 || this.expense === 0) {
-            alert("Must have valid number entries")
-        }
-    }
-}
-//below I created different instances as test runs to see if the Budget class works properly
-let mayBudget = new Budget(10000, 500)
-let decemberBudget = new Budget(29554, 5000)
-let januaryBudget = new Budget(5, 500)
-let augustBudget = new Budget()
-console.log(mayBudget.grandTotal());
-console.log(decemberBudget.grandTotal());
-// console.log(januaryBudget.inputValidation());
-// console.log(augustBudget.inputValidation());
-
-//below I created an one instance 
-let totalAmount = new Budget();
-
-//
-const incomeAmount = totalAmount.income;
-const expenseAmount = totalAmount.expense;
-const grandTotalAmount = incomeAmount - expenseAmount;
-
-//below the code will update the all the total displays from the user's input
-const incomeTotal = document.getElementById("incomeTotal").textContent = incomeAmount;
-const expenseTotal = document.getElementById("expenseTotal").textContent = expenseAmount;
-const grandTotal = document.getElementById("grandTotal").textContent = grandTotalAmount;
-
-//below I'm verifying if the numbers are zero
-console.log(incomeTotal);
-console.log(expenseTotal);
-console.log(grandTotalAmount);
+} 
